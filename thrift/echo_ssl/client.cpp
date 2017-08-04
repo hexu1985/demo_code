dc4b1484455c33ee4c055db95d0b2134da809ac1
hexu <hexu_1985@sina.com>
2017-08-04 18:02:45 +0800

    add thrift/echo_ssl

---
+++ thrift/echo_ssl/client.cpp
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <iostream>
+#include <string>
+
+#include <thrift/protocol/TJSONProtocol.h>
+#include <thrift/transport/TSSLSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "EchoTest.h"
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace demo;
+
+int main(int argc, char** argv) {
+  string host = "localhost";
+  int port = 9090;
+
+  if (argc == 2) {
+    int tmp = atoi(argv[1]);
+    if (tmp != 0) port = tmp;
+  } else if (argc == 3) {
+    host = argv[1];
+    int tmp = atoi(argv[2]);
+    if (tmp != 0) port = tmp;
+  }
+
+  boost::shared_ptr<TSSLSocketFactory> socketFactory = boost::shared_ptr<TSSLSocketFactory>(new TSSLSocketFactory());
+  socketFactory->loadTrustedCertificates("./keys/server.crt");
+  socketFactory->authenticate(true);
+//  socketFactory->loadCertificate("./client.crt");
+//  socketFactory->loadPrivateKey("./client-key.key");
+  socketFactory->ciphers("ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+
+  boost::shared_ptr<TSocket> socket = socketFactory->createSocket(host, port);
+  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  boost::shared_ptr<TProtocol> protocol(new TJSONProtocol(transport));
+  EchoTestClient client(protocol);
+
+  transport->open();
+
+  string str;
+  string ret_str;
+  while (getline(cin, str)) {
+    client.echo(ret_str, str);
+    cerr << ret_str << "\n";
+  }
+
+  transport->close();
+
+  return 0;
+}
