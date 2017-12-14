5a6e1a04831df5698c4881297ac3ae63db524bbb
hexu <hexu_1985@sina.com>
2017-12-14 11:57:19 +0800

    add python sqlalchemy distinct demo

---
+++ python/sqlalchemy/select_distinct.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+from sqlalchemy import and_,or_
+import student
+
+engine = student.engine
+Session = student.Session
+Base = student.Base
+Student = student.Student
+
+session = Session()    #创建session对象
+print session.query(Student.Sdept).distinct().filter(Student.Ssex == 'M').all()
+session.commit()    #数据对象被推到数据库中，此时为Persistent状态
+session.close()    #关闭session对象
+
