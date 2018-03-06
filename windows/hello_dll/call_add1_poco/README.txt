0468352e161dc502bf81e520fcf96282aa5bb29d
hexu <hexu_1985@sina.com>
2018-03-06 17:24:54 +0800

    update windows/hello_dll

---
+++ windows/hello_dll/call_add1_poco/README.txt
@@ -0,0 +1,4 @@
+# make
+# ./main.exe ../dlldemo1/Debug/dlldemo1.dll
+
+
--- windows/hello_dll/dlldemo1/dlldemo1/my_code.cpp
+++ windows/hello_dll/dlldemo1/dlldemo1/my_code.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 #include "my_code.h"
 
+#include <iostream>
+
 int Add(int a, int b)
 {
+	std::cout << "Add in vistual studio" << std::endl;
 	return (a + b);
 }
