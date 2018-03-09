edaf4f5f360bacac5c945c2555a6409428449a17
hexu <hexu_1985@sina.com>
2018-03-09 11:44:39 +0800

    add all windows file

---
+++ windows/matlablib/call_add/call_add/Program.cs
@@ -0,0 +1,21 @@
+﻿using add;
+using MathWorks.MATLAB.NET.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace call_add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            MWArray b = 3, c = 6;
+            MWArray result = a.add(b, c);
+            Console.WriteLine(result.ToString());
+            Console.Read();
+        }
+    }
+}
