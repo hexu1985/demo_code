64769f938b633ec30181f1da23bf60ba2f27b13c
hexu <hexu_1985@sina.com>
2018-04-16 13:13:34 +0800

    add set_parity.cpp

---
+++ serial_port/cpp/set_parity.cpp
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <iostream>
+#include "wraptermios.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 5) {
+        cout << "usage: " << argv[0] << " <ttyname> <databits> <stopbits> <parity>" << endl; 
+        return 1;
+    }
+
+    int fd = Tty_open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
+    cout << "Open tty: " << argv[1] << " ok, " << "fd: " << fd << endl;
+
+    Tty_set_parity(fd, atoi(argv[2]), atoi(argv[3]), argv[4][0]);
+    cout << "Set parity ok, " 
+        << "databits: " << argv[2] << ", "
+        << "stopbits: " << argv[3] << ", "
+        << "parity: " << argv[4] << ". " << endl;
+
+    return 0;
+}
+
+
