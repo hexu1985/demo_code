92e03b5a1926892e4548c38d33ba39bd2dee7c6e
hexu <hexu_1985@sina.com>
2018-06-16 02:48:41 +0800

    rename path -> path_back

--- boost/filesystem/path/filename.cpp
+++ boost/filesystem/path/filename.cpp
@@ -2,18 +2,30 @@
 #include "boost/filesystem.hpp"
 
 namespace fs = boost::filesystem;
-
+ 
 int main()
 {
     std::cout << fs::path("/foo/bar.txt").filename() << '\n'
-            << fs::path("/foo/.bar").filename() << '\n'
-            << fs::path("/foo/bar/").filename() << '\n'
-            << fs::path("/foo/.").filename() << '\n'
-            << fs::path("/foo/..").filename() << '\n'
-            << fs::path(".").filename() << '\n'
-            << fs::path("..").filename() << '\n'
-            << fs::path("/").filename() << '\n'
-            << fs::path("//host").filename() << '\n';
-
-    return 0;
+              << fs::path("/foo/.bar").filename() << '\n'
+              << fs::path("/foo/bar/").filename() << '\n'
+              << fs::path("/foo/.").filename() << '\n'
+              << fs::path("/foo/..").filename() << '\n'
+              << fs::path(".").filename() << '\n'
+              << fs::path("..").filename() << '\n'
+              << fs::path("/").filename() << '\n'
+              << fs::path("//host").filename() << '\n';
 }
+
+/*
+Possible output:
+
+"bar.txt"
+".bar"
+""
+"."
+".."
+"."
+".."
+""
+"host"
+*/
---
+++ boost/filesystem/path_back/parent_path.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "boost/filesystem.hpp"
+
+namespace fs = boost::filesystem;
+
+int main()
+{
+    for(fs::path p : {"/var/tmp/example.txt", "/", "/var/tmp/."})
+        std::cout << "The parent path of " << p
+                  << " is " << p.parent_path() << '\n';
+}
+
+/*
+Possible output:
+
+The parent path of "/var/tmp/example.txt" is "/var/tmp"
+The parent path of "/" is "/"
+The parent path of "/var/tmp/." is "/var/tmp"
+*/
