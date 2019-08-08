01fb5b2b762e758385038f4037ec377427bdd91d
hexu <hexu@sensetime.com>
2019-08-08 17:20:44 +0800

    add cmake/tutorial/s7

---
+++ cmake/tutorial/s7/MathFunctions/mysqrt.cxx
@@ -0,0 +1,10 @@
+#include "MathFunctions.h"
+#include "Table.h"
+#include <cmath>
+#include <iostream>
+
+double mysqrt (double x)
+{
+	std::cout << "mysqrt(" << x << ") called" << std::endl;
+    return std::sqrt(x);
+}
