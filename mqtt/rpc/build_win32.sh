f69e17423548d2abb7399e97f7685de98abcf5e4
hexu <hexu_1985@sina.com>
2019-09-12 15:43:05 +0800

    update mqtt/rpc

--- mqtt/rpc/build_win32.sh
+++ mqtt/rpc/build_win32.sh
@@ -1,3 +1,3 @@
-cmake -G "Visual Studio 14" -Bbuild -H. -DCMAKE_INSTALL_PREFIX="D:\mqtt\x86\mqtt-rpc-client" -DPAHO_WITH_SSL=ON -DPAHO_C_ROOT_DIR="D:\mqtt\x86\paho-c" -DPAHO_CPP_ROOT_DIR="D:\mqtt\x86\paho-cpp" -DOPENSSL_SEARCH_LIB_PATH="C:\OpenSSL-Win32\lib32"
+cmake -G "Visual Studio 14" -Bbuild -H. -DCMAKE_INSTALL_PREFIX="D:\mqtt\x86\mqtt-rpc-client" -DPAHO_WITH_SSL=ON -DPAHO_C_ROOT_DIR="D:\mqtt\x86\paho-c" -DPAHO_CPP_ROOT_DIR="D:\mqtt\x86\paho-cpp" -DOPENSSL_SEARCH_LIB_PATH="C:\OpenSSL-Win32\lib"
 cmake --build build/ --target install
 
