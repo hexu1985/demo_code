ca4e0474b0d4997e5ea32bbb10090ff1b6ce1a5d
hexu <hexu_1985@sina.com>
2019-10-17 17:28:01 +0800

    add dotguide/graph[4-6].dot

--- graphviz/dotguide/build.sh
+++ graphviz/dotguide/build.sh
@@ -1,3 +1,5 @@
 dot graph1.dot -T png -o graph1.png
 dot graph2.dot -T png -o graph2.png
 dot graph3.dot -T png -o graph3.png
+dot graph4.dot -T png -o graph4.png
+dot graph5.dot -T png -o graph5.png
