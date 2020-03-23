7a42903856407c572b2a9b55b6dcf321d3a29e1c
hexu <hexu@sensetime.com>
2020-03-23 14:26:22 +0800

    update nlohmann_json/demo/write.cpp

--- nlohmann_json/demo/write.cpp
+++ nlohmann_json/demo/write.cpp
@@ -14,5 +14,12 @@ int main()
             };
     std::cout << obj.dump() << std::endl;
 
+    std::cout << "========= array ==========" << std::endl;
+    json array = json::array();
+    array.push_back({{"name", "hexu"}, {"weight", 75}});
+    array.push_back({{"name", "xxx"}, {"weight", 77}});
+    array.push_back({{"name", "yyy"}, {"weight", 66}});
+    std::cout << array.dump() << std::endl;
+
 	return 0;
 }
