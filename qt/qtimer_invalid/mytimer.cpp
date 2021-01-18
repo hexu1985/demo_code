442ca8f3b758e8bd5261e20f51f9b69581cedb4a
hexu <hexu_1985@sina.com>
2021-01-18 20:12:24 +0800

    update qtimer_invalid

--- qt/qtimer_invalid/mytimer.cpp
+++ qt/qtimer_invalid/mytimer.cpp
@@ -1,7 +1,7 @@
 #include "mytimer.h"
 #include <QDebug>
 
-MyTimer::MyTimer()
+MyTimer::MyTimer(): thread("test")
 {
     // create a timer
     timer = new QTimer(this);
@@ -11,10 +11,19 @@ MyTimer::MyTimer()
           this, SLOT(MyTimerSlot()));
 
     // msec
-    timer->start(1000);
+    timer->start(3000);
+    thread.start();
+}
+
+void dowork()
+{
+  QTimer::singleShot(1000, []{
+    qDebug() << "dowork...";
+      });
 }
 
 void MyTimer::MyTimerSlot()
 {
     qDebug() << "Timer...";
+    thread.getTaskQueue()->pushTask(&dowork);
 }
--- qt/qtimer_invalid/mytimer.h
+++ qt/qtimer_invalid/mytimer.h
@@ -2,12 +2,15 @@
 #define MYTIMER_H
 
 #include <QTimer>
+#include "WorkerThread.hpp"
 
 class MyTimer : public QObject {
 Q_OBJECT
 public:
     MyTimer();
     QTimer *timer;
+
+    mini_utils::WorkerThread thread;
 
 public slots:
     void MyTimerSlot();
