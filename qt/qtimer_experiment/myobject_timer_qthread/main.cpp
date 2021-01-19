c70c19f170b484695abcc814018d9e7528a06b82
hexu <hexu_1985@sina.com>
2021-01-19 14:37:23 +0800

    update qt/qtimer_experiment/myobject_timer_qthread

--- qt/qtimer_experiment/myobject_timer_qthread/main.cpp
+++ qt/qtimer_experiment/myobject_timer_qthread/main.cpp
@@ -7,28 +7,23 @@
 MyObject *global_object_ptr = 0;
 
 class MyThread : public QThread {
-    Q_OBJECT
 public:
-    MyThread(QObject* parent = nullptr);
-
-#if 0
-    //自定义发送的信号
-signals:
-    void myThreadSignal(const int);
-    //自定义槽
-public slots:
-    void myThreadSlot(const int);
-#endif
+    MyThread();
+    ~MyThread();
 
 protected:
     void run() override;
 };
 
-MyThread::MyThread(QObject *parent)
+MyThread::MyThread()
 {
  
 }
  
+MyThread::~MyThread()
+{
+}
+
 void thread_func() 
 {
     std::cout << "QTimer::singleShot:" << std::endl; 
@@ -46,13 +41,15 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    qDebug()<<"     main thread ID:"<<QThread::currentThreadId()<<'\n';
+
     // Create MyTimer instance
     // QTimer object will be created in the MyTimer constructor
     MyObject my_object;
 
     global_object_ptr = &my_object;
 
-    Thread thr(nullptr);
+    MyThread thr;
 		thr.start();
 
     return a.exec();
