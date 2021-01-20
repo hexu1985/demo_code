8be2205281ae934c85f7def673cb157441e7edbd
hexu <hexu_1985@sina.com>
2021-01-20 19:34:02 +0800

    add xdg_open.cpp

---
+++ system_util/linux/xdg_open.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    system("xdg-open https://www.baidu.com");
+    system("xdg-open /tmp");
+    system("xdg-open /tmp/test.txt");
+    return 0;
+}
