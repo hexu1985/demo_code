de3e1fb47228522ba677aa6de578fbe824296f84
hexu <hexu_1985@sina.com>
2021-01-21 20:05:58 +0800

    add system_util/linux/opendir.cpp

---
+++ system_util/linux/opendir.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+//    system("nautilus /tmp");
+//    system("nautilus /tmp/test.txt");
+    system("nautilus ~/..");
+//    system("nautilus ~");
+    return 0;
+}
+
