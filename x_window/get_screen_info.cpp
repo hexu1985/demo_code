1460b4f59f10fe1d84ca1f3b4ea70d2c8eb92963
hexu <hexu_1985@sina.com>
2021-01-22 16:02:34 +0800

    add/x_window/get_screen_info.cpp

---
+++ x_window/get_screen_info.cpp
@@ -0,0 +1,24 @@
+#include <X11/Xlib.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(int argc, char *agrv[])
+{
+    XSetWindowAttributes winattrs;
+    int swidth;/*屏幕宽度*/
+    int sheight;/*屏幕高度*/
+    int winmask;
+    Display *display;
+    int screen;
+    Window window;
+
+    display = XOpenDisplay(getenv("DISPLAY"));
+    screen = DefaultScreen(display);
+    swidth = DisplayWidth(display, screen);/*获取屏幕宽度*/
+    sheight = DisplayHeight(display, screen);/*获取屏幕高度*/
+
+    std::cout << "screen width: " << swidth << std::endl;
+    std::cout << "screen height: " << sheight << std::endl;
+
+    return 0;
+}
