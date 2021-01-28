eaa0c23200ccfee586f56cfbc3273d96728a406f
hexu <hexu_1985@sina.com>
2021-01-28 18:56:17 +0800

    update x_window/get_screen_info2.cpp

--- x_window/get_screen_info2.cpp
+++ x_window/get_screen_info2.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <stdarg.h>
+#include <string.h>
 
 typedef enum _name_kind {
     name_none = 0,
@@ -48,19 +49,70 @@ struct _crtc {
 static Display *dpy;
 static Window root;
 static int screen = -1;
+
+static crtc_t   *crtcs;
+static int  num_crtcs;
 static XRRScreenResources  *res;
 static int  minWidth, maxWidth, minHeight, maxHeight;
 
 static void fatal(const char *format, ...)
 {
     va_list ap;
-  
+
     va_start(ap, format);
     vfprintf(stderr, format, ap);
     va_end(ap);
     exit(1);
     /*NOTREACHED*/
 }
+
+static void set_name_xid (name_t *name, XID xid)
+{
+    int kind = name->kind;
+    kind |= name_xid;
+    name->kind = static_cast<name_kind_t>(kind);
+    name->xid = xid;
+}
+
+static void set_name_index (name_t *name, int index)
+{
+    int kind = name->kind;
+    kind |= name_index;
+    name->kind = static_cast<name_kind_t>(kind);
+    name->index = index;
+}
+
+static void init_transform (transform_t *transform)
+{
+    int x; 
+    memset (&transform->transform, '\0', sizeof (transform->transform));
+    for (x = 0; x < 3; x++)
+        transform->transform.matrix[x][x] = XDoubleToFixed (1.0);
+    transform->filter = "";
+    transform->nparams = 0;
+    transform->params = NULL;
+}
+
+static void set_transform (transform_t  *dest,
+        XTransform   *transform,
+        const char   *filter,
+        XFixed      *params,
+        int     nparams)
+{
+    dest->transform = *transform;
+    /* note: this string is leaked */
+    dest->filter = strdup (filter);
+    dest->nparams = nparams;
+    dest->params = static_cast<XFixed *> (malloc (nparams * sizeof (XFixed)));
+    memcpy (dest->params, params, nparams * sizeof (XFixed));
+}
+
+static void copy_transform (transform_t *dest, transform_t *src)
+{
+    set_transform (dest, &src->transform,
+            src->filter, src->params, src->nparams);
+}
+
 
 static void get_screen (bool current)
 {
@@ -71,6 +123,59 @@ static void get_screen (bool current)
     else 
         res = XRRGetScreenResources (dpy, root);
     if (!res) fatal ("could not get screen resources");
+}
+
+static void get_crtcs()
+{
+    int		c;
+
+    num_crtcs = res->ncrtc;
+    crtcs = static_cast<crtc_t *>(calloc (num_crtcs, sizeof (crtc_t)));
+    if (!crtcs) fatal ("out of memory\n");
+
+    for (c = 0; c < res->ncrtc; c++)
+    {
+        XRRCrtcInfo *crtc_info = XRRGetCrtcInfo (dpy, res, res->crtcs[c]);
+        XRRCrtcTransformAttributes  *attr;
+        XRRPanning  *panning_info = NULL;
+
+#if 0
+        XRRPanning zero;
+        memset(&zero, 0, sizeof(zero));
+        panning_info = XRRGetPanning  (dpy, res, res->crtcs[c]);
+        zero.timestamp = panning_info->timestamp;
+        if (!memcmp(panning_info, &zero, sizeof(zero))) {
+            Xfree(panning_info);
+            panning_info = NULL;
+        }
+#endif
+
+        set_name_xid (&crtcs[c].crtc, res->crtcs[c]);
+        set_name_index (&crtcs[c].crtc, c);
+        if (!crtc_info) fatal ("could not get crtc 0x%x information\n", res->crtcs[c]);
+        crtcs[c].crtc_info = crtc_info;
+        crtcs[c].panning_info = panning_info;
+        if (crtc_info->mode == None)
+        {
+            crtcs[c].mode_info = NULL;
+            crtcs[c].x = 0;
+            crtcs[c].y = 0;
+            crtcs[c].rotation = RR_Rotate_0;
+        }
+        if (XRRGetCrtcTransform (dpy, res->crtcs[c], &attr) && attr) {
+            set_transform (&crtcs[c].current_transform,
+                    &attr->currentTransform,
+                    attr->currentFilter,
+                    attr->currentParams,
+                    attr->currentNparams);
+            XFree (attr);
+        }
+        else
+        {
+            init_transform (&crtcs[c].current_transform);
+        }
+        copy_transform (&crtcs[c].pending_transform, &crtcs[c].current_transform);
+    }
 }
 
 int main(int argc, char *agrv[])
@@ -86,9 +191,9 @@ int main(int argc, char *agrv[])
 
     get_screen(true);
     printf ("Screen %d: minimum %d x %d, current %d x %d, maximum %d x %d\n",
-        screen, minWidth, minHeight,
-        DisplayWidth (dpy, screen), DisplayHeight(dpy, screen),
-        maxWidth, maxHeight);
+            screen, minWidth, minHeight,
+            DisplayWidth (dpy, screen), DisplayHeight(dpy, screen),
+            maxWidth, maxHeight);
 
 
     return 0;
