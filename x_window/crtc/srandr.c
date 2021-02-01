da9236376c7b580b1ce936ac921fbb2f8eab1870
hexu <hexu_1985@sina.com>
2021-02-01 20:37:44 +0800

    update srandr.c

--- x_window/crtc/srandr.c
+++ x_window/crtc/srandr.c
@@ -13,6 +13,22 @@
 #include <stdarg.h>
 #include <math.h>
 
+const char* RRNotifySubtypeToString(int subtype) {
+    switch (subtype) {
+        case RRNotify_OutputChange:
+            return "RRNotify_OutputChange";
+        case RRNotify_CrtcChange:
+            return "RRNotify_CrtcChange";
+        case RRNotify_OutputProperty:
+            return "RRNotify_OutputProperty";
+        default:
+            return "Unknown RRNotify";
+    }
+}
+
+int		rr_event_base_, rr_error_base_;
+int		major, minor;
+
 int process_events(Display * dpy)
 {
 	XEvent ev;
@@ -22,7 +38,22 @@ int process_events(Display * dpy)
 //	XSetIOErrorHandler((XIOErrorHandler) error_handler);
 	while (1) {
 		if (!XNextEvent(dpy, &ev)) {
-            printf("get some event\n");
+			XEvent* xevent = &ev;
+			XRRNotifyEvent *aevent = (XRRNotifyEvent*) (xevent);
+			if (xevent->type == rr_event_base_ + RRScreenChangeNotify ||
+					xevent->type == rr_event_base_ + RRNotify) {
+				switch (xevent->type - rr_event_base_) {
+					case RRScreenChangeNotify:
+						printf("RRScreenChangeNotify event received.\n");
+						XRRUpdateConfiguration(xevent);
+						break;
+					case RRNotify:
+						printf("%s event received.\n", RRNotifySubtypeToString(aevent->subtype));
+						break;
+					default:
+						printf("Unknown GDK event received.\n");
+				}
+            }
 		}
 	}
 	return EXIT_SUCCESS;
@@ -33,8 +64,6 @@ int main()
     Display	*dpy;
     int	screen = -1;
     char          *display_name = NULL;
-    int		event_base, error_base;
-    int		major, minor;
 
     dpy = XOpenDisplay (display_name);
 
@@ -50,7 +79,7 @@ int main()
         exit (1);
     }
 
-    if (!XRRQueryExtension (dpy, &event_base, &error_base) ||
+    if (!XRRQueryExtension (dpy, &rr_event_base_, &rr_error_base_) ||
             !XRRQueryVersion (dpy, &major, &minor))
     {
         fprintf (stderr, "RandR extension missing\n");
