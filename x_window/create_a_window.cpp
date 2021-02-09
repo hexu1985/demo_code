e7024586130b6514c449c8011bd8f108a9f5dbd2
hexu <hexu_1985@sina.com>
2021-02-09 17:05:24 +0800

    update create_a_window.cpp

--- x_window/create_a_window.cpp
+++ x_window/create_a_window.cpp
@@ -33,7 +33,7 @@ int main(void)
                            BlackPixel(display, s), WhitePixel(display, s));
  
     /* 选择一种感兴趣的事件进行监听 */
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
  
     /* 显示窗口 */
     XMapWindow(display, window);
@@ -46,9 +46,20 @@ int main(void)
         /* 绘制窗口或者重新绘制 */
         if (event.type == Expose)
         {
+            printf("receive Expose event\n");
             XFillRectangle(display, window, DefaultGC(display, s), 20, 20, 10, 10);
             XDrawString(display, window, DefaultGC(display, s), 50, 50, msg, strlen(msg));
         }
+
+        if (event.type == ConfigureNotify){
+            XConfigureEvent xce = event.xconfigure;
+
+            /* This event type is generated for a variety of
+               happenings, so check whether the window has been
+               resized. */
+            fprintf(stderr, "New Width,Height %d,%d\n", xce.width, xce.height);
+        }
+
         /* 当检测到键盘按键,退出消息循环 */
         if (event.type == KeyPress)
             break;
