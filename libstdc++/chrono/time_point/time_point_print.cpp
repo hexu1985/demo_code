2e1d8bcc12db5c703a07afc6213547ece4db8931
hexu <hexu_1985@sina.com>
2021-03-08 10:55:41 +0800

    add libstdc++/chrono/time_point/time_point_print

---
+++ libstdc++/chrono/time_point/time_point_print.cpp
@@ -0,0 +1,27 @@
+#include <iomanip>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+template<typename Clock, typename Duration>
+std::ostream &operator<<(std::ostream &stream,
+  const std::chrono::time_point<Clock, Duration> &time_point) {
+  const time_t time = Clock::to_time_t(time_point);
+#if __GNUC__ > 4 || \
+    ((__GNUC__ == 4) && __GNUC_MINOR__ > 8 && __GNUC_REVISION__ > 1)
+  // Maybe the put_time will be implemented later?
+  struct tm tm;
+  localtime_r(&time, &tm);
+  return stream << std::put_time(&tm, "%c"); // Print standard date&time
+#else
+  char buffer[26];
+  ctime_r(&time, buffer);
+  buffer[24] = '\0';  // Removes the newline that is added
+  return stream << buffer;
+#endif
+}
+
+int main() {
+  std::cout << std::chrono::system_clock::now() << std::endl;
+  // Wed May 22 14:17:03 2013
+}
