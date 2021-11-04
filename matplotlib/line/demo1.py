eb2d242ce0134468c1e7450263b57cc87f6aff2d
hexu <hexu_1985@sina.com>
2021-11-04 16:14:43 +0800

    add matplotlib/line

---
+++ matplotlib/line/demo1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([-2,-1,0,1,2])
+y = np.array([-3,-1,1,3,5])
+
+plt.plot(x, y, marker = 'o')
+plt.show()
---
+++ matplotlib/line/demo2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([-2,-1,0,1,2])
+y = np.array([-3,-1,1,3,5])
+
+plt.plot(x, y, marker = 'o', label="y=2x+1")
+plt.legend()
+plt.show()
