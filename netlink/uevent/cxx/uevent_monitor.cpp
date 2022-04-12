b48ffb2ed67d72d1a80a234350501b14ffd20288
hexu <hexu_1985@sina.com>
2022-04-12 18:52:52 +0800

    update uevent_monitor.cpp

--- netlink/uevent/cxx/uevent_monitor.cpp
+++ netlink/uevent/cxx/uevent_monitor.cpp
@@ -11,7 +11,80 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <chrono>
+#include <tuple>
+#include <set>
 //#include <linux/rtnetlink.h>
+
+struct Uevent {
+    std::string action;
+    std::string devpath;
+    std::string subsystem;
+    std::string devname;
+    std::string devtype;
+    std::chrono::steady_clock::time_point timestamp;
+};
+
+void print(const Uevent& uevent)
+{
+    std::cout << "action: " << uevent.action << ", "
+              << "devpath: " << uevent.devpath << ", "
+              << "subsystem: " << uevent.subsystem << ", "
+              << "devname: " << uevent.devname << ", "
+              << "devtype: " << uevent.devtype << std::endl;
+}
+
+std::tuple<std::string, std::string> split_line(const std::string& line)
+{
+    size_t pos = line.find('=');
+    if (pos == std::string::npos)
+        return std::tuple<std::string, std::string>();
+    else
+        return std::make_tuple(line.substr(0, pos), line.substr(pos+1));
+}
+
+bool str2Uevent(const std::string& data, std::chrono::steady_clock::time_point timestamp, Uevent& uevent)
+{
+    std::string line;
+    std::istringstream is;
+    is.str(data);
+    std::string key;
+    std::string value;
+    std::set<std::string> key_set;
+    while (std::getline(is, line)) {
+        std::tie(key, value) = split_line(line);
+        if (key.empty()) {
+            std::cout << "key is empty" << std::endl;
+            continue;
+        }
+        if (key == "ACTION") {
+            uevent.action = value;
+            key_set.insert(key);
+        } else if (key == "DEVPATH") {
+            uevent.devpath = value;
+            key_set.insert(key);
+        } else if (key == "SUBSYSTEM") {
+            uevent.subsystem = value;
+            key_set.insert(key);
+        } else if (key == "DEVNAME") {
+            uevent.devname = value;
+            key_set.insert(key);
+        } else if (key == "DEVTYPE") {
+            uevent.devtype = value;
+            key_set.insert(key);
+        } else {
+            // do nothing
+        }
+    }
+
+    if (key_set.size() != 5) {
+        return false;
+    }
+
+    uevent.timestamp = timestamp;
+    return true;
+}
+
 
 void MonitorNetlinkUevent()
 {
@@ -34,11 +107,12 @@ void MonitorNetlinkUevent()
 
     int count = 0;
     std::string data;
-    std::string line;
-    std::istringstream is;
+    Uevent uevent;
+    std::chrono::steady_clock::time_point now;
     for (;;) {
         memset(buf,0,4096);
         len=recv(sockfd,buf,4096,0);
+        now = std::chrono::steady_clock::now();
         if(len<0) {
             printf("receive error\n");
             continue;
@@ -64,11 +138,10 @@ void MonitorNetlinkUevent()
             continue;
         }
 
-        is.clear();
-        is.str(data.substr(pos));
-        while (std::getline(is, line)) {
-            std::cout << "line: " << line << std::endl;
+        if (str2Uevent(data.substr(pos), now, uevent)) {
+            print(uevent);
         }
+
         std::cout << "received " << len << " bytes" << std::endl;
         std::cout << data.substr(pos) << std::endl;
         printf("***********************msg %d ends************************\n", count);
