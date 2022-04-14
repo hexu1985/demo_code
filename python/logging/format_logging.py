b2b522d6c0684a6d94e9f2899c48272218899a1e
hexu <hexu_1985@sina.com>
2022-04-14 16:50:42 +0800

    add python/logging

---
+++ python/logging/format_logging.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import logging.config
+import os
+import datetime
+
+# logging
+LOG_ROOT = "."
+LOG_FILE = os.path.join(
+    LOG_ROOT,
+    "test.log.%s" %
+    datetime.datetime.now().strftime('%Y%m%d-%H%M%S.%f')
+)
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format":
+                ("[%(levelname)s][%(asctime)s][%(threadName)s:%(thread)d]"
+                 "[%(filename)s:%(lineno)d] %(message)s")
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard"
+        },
+        "default": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_FILE,
+            "maxBytes": 1024 * 1024 * 30,
+            "backupCount": 3,
+            "formatter": "standard",
+            "encoding": "utf-8",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console", "default"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING)
+LOGGER = logging.getLogger()
+
+if __name__ == '__main__':
+    LOGGER.info("hello info")
+    LOGGER.error("hello error")
