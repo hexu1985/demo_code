b03cfe060d0539cb9cdca6d1442a9ae9be6f0cdc
hexu <hexu_1985@sina.com>
2022-04-18 18:25:35 +0800

    update get_exefile_list.py

--- python/pathlib/get_exefile_list.py
+++ python/pathlib/get_exefile_list.py
@@ -5,9 +5,9 @@ import os
 import sys
 
 
-def get_exefile_lst(dir_path):
+def get_exefile_list(dir_path):
     exefile_lst = []
-    p = pathlib.Path(dir_path)
+    p = pathlib.Path(dir_path).resolve()
     for f in p.glob("*"):
         if os.access(f, os.X_OK):
             exefile_lst.append(f)
@@ -19,5 +19,7 @@ if __name__ == "__main__":
         dir_path = sys.argv[1]
 
     print("dir_path: {}".format(dir_path))
-    cmd_lst = get_exefile_lst(dir_path)
+    cmd_lst = get_exefile_list(dir_path)
     print("cmd_lst: {}".format(cmd_lst))
+    for cmd in cmd_lst:
+        print("cmd {}".format(cmd))
