6ec0ddee6a60d5424e8e18856817a3f8b94c9cb9
hexu <hexu_1985@sina.com>
2022-04-18 17:45:48 +0800

    add get_exefile_list.py

---
+++ python/pathlib/get_exefile_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import pathlib
+import os
+import sys
+
+
+def get_exefile_lst(dir_path):
+    exefile_lst = []
+    p = pathlib.Path(dir_path)
+    for f in p.glob("*"):
+        if os.access(f, os.X_OK):
+            exefile_lst.append(f)
+    return exefile_lst
+
+if __name__ == "__main__":
+    dir_path = "."
+    if len(sys.argv) > 1:
+        dir_path = sys.argv[1]
+
+    print("dir_path: {}".format(dir_path))
+    cmd_lst = get_exefile_lst(dir_path)
+    print("cmd_lst: {}".format(cmd_lst))
