e92babf9adb8be1130b2a9acf7c53f821d102a51
hexu <hexu_1985@sina.com>
2022-04-21 11:26:14 +0800

    add pathlib/mkdir_if_not_exists.py

---
+++ python/pathlib/mkdir_if_not_exists.py
@@ -0,0 +1,29 @@
+import pathlib
+import logging
+import os
+
+LOGGER = logging.getLogger()
+
+def mkdir_if_not_exists(dir_path):
+    path = pathlib.Path(dir_path)
+    if path.exists():
+        if not path.is_dir():
+            LOGGER.error('path "{}" already exist but not dir'.format(path))
+            return False
+        elif not os.access(dir_path, os.W_OK):
+            LOGGER.error('path "{}" already exist but not write permission'.format(path))
+            return False
+        else:
+            return True
+    else:
+        path.mkdir()
+        return True
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    if mkdir_if_not_exists("./log"):
+        LOGGER.info("mkdir ok")
+    else:
+        LOGGER.info("mkdir failed")
+        
