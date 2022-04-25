4bf2907cb32f2bc92a0fb0c3e3e0c570ccb26f1b
hexu <hexu_1985@sina.com>
2022-04-25 15:23:47 +0800

    add bash/test_dir/test_dir.sh

---
+++ bash/test_dir/test_dir.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo $#
+if [ $# -lt 1 ]
+then
+    echo "usage $0 dir"
+    exit 1
+fi
+
+test_dir=$1
+
+if [ ! -e "$test_dir" ]
+then
+    echo "$test_dir is no exists"
+    exit 1
+fi
+
+if [ ! -d "$test_dir" ]
+then
+    echo "$test_dir is no dir"
+    exit 1
+fi
+
+if [ -z `ls -A $test_dir` ]
+then
+    echo "$test_dir is empty dir"
+    exit 1
+fi
+
+echo "$test_dir is dir and no empty"
