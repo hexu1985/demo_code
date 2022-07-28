1bb467d967fdc1fb92a2cb747613b8483154e53f
hexu <hexu_1985@sina.com>
2022-07-28 14:32:45 +0800

    update python/json

--- python/json/dumps.py
+++ python/json/dumps.py
@@ -4,4 +4,4 @@ import json
 data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
 
 json = json.dumps(data)
-print json
+print(json)
--- python/json/loads.py
+++ python/json/loads.py
@@ -4,4 +4,4 @@ import json
 jsonData = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 
 text = json.loads(jsonData)
-print text
+print(text)
