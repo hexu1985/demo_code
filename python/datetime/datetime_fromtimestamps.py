b7bacb5ba71ab7b44e9a1659c620ba51dbacdca8
hexu <hexu_1985@sina.com>
2022-08-09 14:39:08 +0800

    update python/datetime

---
+++ python/datetime/datetime_fromtimestamps.py
@@ -0,0 +1,7 @@
+import datetime
+import time
+
+ts = 1658124616 # 时间戳
+dt = datetime.datetime.fromtimestamp(ts)
+
+print(dt) 
---
+++ python/datetime/datetime_timestamps.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+
+dt = datetime.datetime.now()
+ts = dt.timestamp()
+print(dt) # datetime.datetime(2019, 9, 11, 11, 20, 6, 681320)
+print(ts) # 1568172006.68132
+
