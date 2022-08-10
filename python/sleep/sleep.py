b1f34b7955f51f2e708b973da5052efc2860e689
hexu <hexu_1985@sina.com>
2022-08-10 16:28:50 +0800

    add python/sleep

---
+++ python/sleep/sleep.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import time
+from datetime import datetime
+
+print("now: {}".format(datetime.now()))
+print("sleep 3s")
+time.sleep(3.0)
+print("now: {}".format(datetime.now()))
+
+
