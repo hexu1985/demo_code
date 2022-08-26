a4f7c7d460d396a3215b2dc21f8554ace82f1975
hexu <hexu_1985@sina.com>
2022-08-26 11:14:37 +0800

    add python/logging/set_loggin_format.py

---
+++ python/logging/set_logging_format.py
@@ -0,0 +1,14 @@
+import logging
+
+LOGGER = logging.getLogger()
+
+def print_log():
+    LOGGER.info("print info log")
+    LOGGER.error("print error log")
+    LOGGER.warning("print warning log")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format=("[%(levelname)s][%(asctime)s][%(threadName)s:%(thread)d]"
+                     "[%(filename)s:%(lineno)d] %(message)s"))
+    print_log()
