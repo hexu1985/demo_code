811e7badb366fdf1a3614b02c25f2d37be87e8e9
hexu <hexu_1985@sina.com>
2022-09-29 11:51:42 +0800

    update findall_datetime.py

--- python/regex/findall_datetime.py
+++ python/regex/findall_datetime.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import re
+import datetime
 
 def findall_datetime(line):
     pattern = '\\.'+'[0-9]'*8+'-'+'[0-9]'*6+'\\.'
     print("line: {}".format(line))
     for results in re.findall(pattern, line):
         print(results[1:-1])
+        datestr = results[1:-1]
+        dt = datetime.datetime.strptime(datestr, "%Y%m%d-%H%M%S")
+        print(dt)
 
 if __name__ == "__main__":
     findall_datetime('safety_collision.log.info.20220912-094628.22874')
