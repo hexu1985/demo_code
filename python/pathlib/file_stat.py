d1de46179bd23493cd8b3a4f29006feac78dcb93
hexu <hexu_1985@sina.com>
2022-09-29 16:51:53 +0800

    add pathlib/file_stat.py

---
+++ python/pathlib/file_stat.py
@@ -0,0 +1,13 @@
+import pathlib
+import datetime
+
+def create_datetime(ts):
+    return datetime.datetime.fromtimestamp(ts)
+
+current_path = pathlib.Path(__file__)
+print("file: {}".format(current_path))
+print("stat: {}".format(current_path.stat()))
+stat = current_path.stat()
+print("atime: {}".format(create_datetime(stat.st_atime)))
+print("mtime: {}".format(create_datetime(stat.st_mtime)))
+print("ctime: {}".format(create_datetime(stat.st_ctime)))
