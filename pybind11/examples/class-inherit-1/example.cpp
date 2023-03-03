935ed14757a877fd0f43198c940d971b306b7471
hexu <hexu_1985@sina.com>
2023-03-03 14:57:34 +0800

    add pybind11/examples/class-inherit-1

---
+++ pybind11/examples/class-inherit-1/example.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+class Animal {
+public:
+    virtual ~Animal() { }
+    virtual std::string go(int n_times) = 0;
+};
+
+class Dog : public Animal {
+public:
+    std::string go(int n_times) override {
+        std::string result;
+        for (int i=0; i<n_times; ++i)
+            result += "woof! ";
+        return result;
+    }
+};
+
+std::string call_go(Animal *animal) {
+    return animal->go(3);
+}
+
+PYBIND11_MODULE(example, m) {
+    py::class_<Animal>(m, "Animal")
+        .def("go", &Animal::go);
+
+    py::class_<Dog, Animal>(m, "Dog")
+        .def(py::init<>());
+
+    m.def("call_go", &call_go);
+}
---
+++ pybind11/examples/class-inherit-1/test1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from example import *
+
+d = Dog()
+print(call_go(d))
+
