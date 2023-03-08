570e06743257e6b42d085242b3d683c9609689c2
hexu <hexu_1985@sina.com>
2023-03-08 14:45:36 +0800

    update mqtt/python

---
+++ mqtt/python/pub.py
@@ -0,0 +1,65 @@
+# python 3.6
+
+import random
+import time
+
+from argparse import ArgumentParser
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+#   client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='use MQTT publish a message')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help='IP address of broker (default: %(default)s)')
+    parser.add_argument('--port', '-p', type=int, default=1883, help='port number of broker (default: %(default)s)')
+    args = parser.parse_args()
+
+    print("args: {}".format(args))
+
+    broker = args.host
+    port = args.port
+
+    run()
+
---
+++ mqtt/python/publish_circular.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser
+from datetime import datetime
+import os
+import time
+import paho.mqtt.client as mqtt
+
+def publish(broker="127.0.0.1", port=1883):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    def on_publish(client,userdata,result):             #create function for callback
+        print("{}: data published: {}, {}, {} \n".format(
+            datetime.now(), client, userdata, result))
+
+    def on_disconnect(client, userdata, result):
+        print("{}: on disconnect: {}, {}, {} \n".format(
+            datetime.now(), client, userdata, result))
+
+    client1= mqtt.Client("publisher.{}".format(os.getpid()))                           #create client object
+    client1.on_connect = on_connect
+    client1.on_publish = on_publish                          #assign function to callback
+    client1.on_disconnect = on_disconnect
+    client1.connect(broker,port)                                 #establish connection
+    client1.loop_start()
+    topic = "hello-mqtt"
+
+    while True:
+        message = "The time is {}".format(datetime.now())
+        ret= client1.publish(topic, message)                   #publish
+        if ret[0] != 0:
+            print("Failed to send message")
+        time.sleep(120)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='use MQTT publish a message')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help='IP address of broker (default: %(default)s)')
+    parser.add_argument('--port', '-p', type=int, default=1883, help='port number of broker (default: %(default)s)')
+    args = parser.parse_args()
+
+    print("args: {}".format(args))
+
+    publish(args.host, args.port)
+
