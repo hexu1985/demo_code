cff9a72fca6264db5ed57ea66e9050487267806a
hexu <hexu_1985@sina.com>
2023-04-24 15:43:45 +0800

    add libstdc++/numeric_limits/int_max.cpp

---
+++ libstdc++/numeric_limits/int_max.cpp
@@ -0,0 +1,9 @@
+#include <climits>
+#include <limits>
+#include <iostream>
+
+int main() {
+    std::cout << "INT_MAX: " << INT_MAX << std::endl;
+    std::cout << "numeric_limits<int>::max(): " << std::numeric_limits<int>::max() << std::endl;
+}
+
