e06843ed4360f0a9514f16477b03fe0458936262
hexu <hexu_1985@sina.com>
2023-07-13 11:09:51 +0800

    update libstdc++/unique_ptr/custom_deleter.cpp

--- libstdc++/unique_ptr/custom_deleter.cpp
+++ libstdc++/unique_ptr/custom_deleter.cpp
@@ -11,7 +11,8 @@ public:
 
 int main()
 {
-    std::unique_ptr<int> u1(new int[5]);
-    std::unique_ptr<int, malloc_deleter<int>> u2(new int[5]);
-    std::unique_ptr<int[], malloc_deleter<int>> u3(new int[5]);
+    std::unique_ptr<int> u1(new int[5]);                // error: alloc_dealloc_mismatch
+    std::unique_ptr<int, malloc_deleter<int>> u2(new int[5]);   // error: alloc_dealloc_mismatch
+    std::unique_ptr<int, malloc_deleter<int>> u3((int*)malloc(sizeof(int)*5));
+    std::unique_ptr<int[], malloc_deleter<int>> u4((int*)malloc(sizeof(int)*5));
 }
